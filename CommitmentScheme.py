5d809d97386c145f9f3a947f57a787a706ed564b
Mattias Petersson <Mattias.Petersson93@gmail.com>
2024-02-13 16:05:31 +0100

    Create gitignore, added docstring for commitment scheme.
    Also, default parameters for q and N given that we use Dilithium
    specs.

--- CommitmentScheme.py
+++ CommitmentScheme.py
@@ -1,31 +1,42 @@
+from typing import Tuple
 
-# Temporary types to ensure that typing works (3.12).
-type CommitmentKey = int 
+# Temporary types to ensure that typing works.
+type CommitmentKey = int
 type Com = int
 type Tck = int
 type Td = int
 
+
 class CommitmentScheme:
-    #q for the ring modulus, N for the degree of the irreducible polynomial. 
-    def __init__(self, q, N):
+    """
+    A trapdoor commitment scheme. q and n are constant values no matter what
+    level of NIST security level we use in accordance with Dilithium
+    specifications.
+
+        :param q int: The ring modulus, default 2^23 - 2^13 + 1.
+        :param n int: The degree of the irreducible polynomial, default 256.
+
+    """
+
+    def __init__(self, q: int = 8380417, N: int = 256):
         print(q, N)
 
     def c_gen(self) -> CommitmentKey:
         return 0
-    
+
     def com(self, m, msg) -> Com:
         return 0
-    
+
     def open(self, com, r, msg) -> bool:
-        #return 1 if com, r, msg is valid, 0 otherwise
-        return 0
-    
-    def tc_gen(self) -> (Tck, Td):
+        # return True if (com, r, msg) is valid, False otherwise
+        return False
+
+    def tc_gen(self) -> Tuple[Tck, Td]:
         return (0, 0)
-    
+
     def t_com(self, td) -> Com:
-        # same type of com? Look into. 
+        # same type of com? Look into.
         return 0
-    
+
     def eqv(self, td, com, m):
         return 0
