b5efcfdc03cc95e2e7c3f258ea22f48f8dda137f
Mattias Petersson <ma8884pe-s@student.lu.se>
2024-02-15 13:35:59 +0100

    Commit and open now works for commitment scheme
    All work for this commit is done collaboratively together with Max
    Gustafsson.

--- CommitmentScheme.py
+++ CommitmentScheme.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from BasicFunctions import sampleUniform
 
@@ -8,10 +9,15 @@ class CommitmentScheme:
     """
     A basic commitment scheme. q is chosen such that q = 2d + 1 mod (4d), 
     with d = 8. q is also set to be a prime approximately equal to 2^32.
+    All params are over R_q. 
 
+    :param l int: Dimension of the message space.
+    :param k int: Width of the commitment matrices.
+    :param n int: Height of the commitment matrix A1.
+    :param q int: Prime modulus defining R_q. 
     """
 
-    def __init__(self, l: int = 1, k: int = 3, q: int = 2 ^ 32 - 527, n: int = 1):
+    def __init__(self, l: int = 1, k: int = 3, n: int = 1, q: int = 2 ** 32 - 527):
         self.l = l
         self.k = k
         self.q = q
@@ -24,10 +30,10 @@ class CommitmentScheme:
         A2prime = np.zeros((k-n-l, n))
 
         for i in range(0, k-n-l):
-            A1prime[i] = sampleUniform(*(self.distributionValues))
-            A2prime[i] = sampleUniform(*(self.distributionValues))
+            A1prime[i] = sampleUniform(*self.distributionValues)
+            A2prime[i] = sampleUniform(*self.distributionValues)
         for i in range(k-n-l, k-n):
-            A1prime[i] = sampleUniform(*(self.distributionValues))
+            A1prime[i] = sampleUniform(*self.distributionValues)
 
         ident = np.identity(n)
         for i in range(0, n):
@@ -47,25 +53,78 @@ class CommitmentScheme:
     def __str__(self):
         return "A1: " + str(self.A1) + "\n A2: " + str(self.A2)
 
-    def commit(self, x):
+    def getL(self) -> int:
+        return self.l
+
+    def checkRandomness(self, randomness: np.ndarray) -> bool:
+        """
+        Ensures that the randomness selected is within allowed bounds.
+        ___
+
+        #TODO: Decide if this should be made private or not. 
+        sigma = 11 * kappa * Beta * sqrt(k * N)
+        """
+        N = 1024
+        sigma = 11 * 36 * 1 * math.sqrt(self.k * N)
+        bound = 4 * sigma * math.sqrt(N)
+        for i in randomness:
+            if math.sqrt(np.sum(i)) > bound:
+                return False
+        return True
+
+    def getR(self) -> np.ndarray:
+        _, *dist = self.distributionValues
+        allowedNorm = False
+        randomness = np.zeros(self.k)
+        while not allowedNorm:
+            randomness = sampleUniform(self.k, *dist)
+            allowedNorm = self.checkRandomness(randomness)
+        return randomness
+
+    def commit(self, message: np.ndarray, r: np.ndarray) -> np.ndarray:
         """
         Commit to message x. 
         """
-        dist = self.distributionValues
-        r = sampleUniform(*(dist))
-        C = np.concatenate((self.A1, self.A2))
-        message = np.concatenate((np.zeros(self.n), x))
+        C = np.transpose(np.hstack((self.A1, self.A2)))
         C = np.matmul(C, r)
-        C += message
-        return C
+        message = np.transpose(np.hstack((np.zeros(self.n), message)))
+        return np.add(C, message)
 
-    def open(self):
-        pass
+    def open(self, C, r, x):
+        Xtend = np.zeros(self.n + self.l)
+        for i in range(self.n, self.n+self.l):
+            Xtend[i] = x[i-self.n]
+        f = 0
+        while f == 0:
+            f1 = sampleUniform(1, 0, 1, self.q)
+            f2 = sampleUniform(1, 0, 1, self.q)
+            # TODO: Add check so that f1 and f2 are not the same
+            f = np.subtract(f1, f2)
+        # incorrect
+        print(f)
+        A = np.hstack((self.A1, self.A2))
+        Comp1 = np.matmul(np.transpose(A), r) + f * np.transpose(Xtend)
+        Comp2 = f * C
+        print(Comp1)
+        print(Comp2)
+        equals = True
+        for i in range(len(Comp1)):
+            if Comp1[i] != Comp2[i]:
+                equals = False
+        return equals
 
 
-B = CommitmentScheme()
+def main():
+    B = CommitmentScheme()
+    l = B.getL()
+    message = np.array([1], ndmin=l)
+    randomness = B.getR()
+    com = B.commit(message, randomness)
+    print("Commitment: \n", com, "\n")
+    opening = B.open(com, randomness, message)
+    print("Open: \n", opening)
+    # B.commit(x)
 
-keys = B
-print(B)
-x = np.zeros(1)
-print(B.commit(x))
+
+if __name__ == "__main__":
+    main()
