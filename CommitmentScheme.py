a3e07ca8900dfdd23218c4cf9d586fd726d8e8f8
Mattias Petersson <Mattias.Petersson93@gmail.com>
2024-02-19 10:24:05 +0100

    Refactored CommitmentScheme
    Commitment scheme has been broken out into modules, works for given x,
    r, and f=1. Not for f as a polynomial.

--- CommitmentScheme.py
+++ CommitmentScheme.py
@@ -1,8 +1,7 @@
 import math
 import numpy as np
-from BasicFunctions import sampleUniform
-
-type PP = tuple[np.ndarray, np.ndarray]
+from Distributions import sampleUniform
+import numpy.linalg as lin
 
 
 class CommitmentScheme:
@@ -18,122 +17,135 @@ class CommitmentScheme:
     """
 
     def __init__(self, l: int = 1, k: int = 3, n: int = 1, q: int = 2 ** 32 - 527):
+        def __makeA1(self):
+            A1prime = sampleUniform((n, k-n), *self.uniformValues)
+            return np.concatenate((np.identity(n), A1prime), axis=1)
+
+        def __makeA2(self):
+            A2prime = sampleUniform((l, k-n-l), *self.uniformValues)
+            return np.concatenate(
+                (np.zeros((l, n)), np.identity(l), A2prime), axis=1)
+
         self.l = l
         self.k = k
         self.q = q
         self.n = n
-        self.distributionValues = (n, 0, (q-1)/2, q)
+        self.uniformValues = ((q-1)/2, (q-1)/2, q)
         self.N = 1024
-        self.sigma = 11 * 36 * 1 * math.sqrt(self.k * self.N)
-
-        self.A1 = np.zeros((k, n))
-        self.A2 = np.zeros((k, l))
-        A1prime = np.zeros((k-n, n))
-        A2prime = np.zeros((k-n-l, n))
-
-        for i in range(0, k-n-l):
-            A1prime[i] = sampleUniform(*self.distributionValues)
-            A2prime[i] = sampleUniform(*self.distributionValues) 
-        for i in range(k-n-l, k-n):
-            A1prime[i] = sampleUniform(*self.distributionValues) 
-
-        ident = np.identity(n)
-        for i in range(0, n):
-            self.A1[i] = ident[i] 
-        for i in range(n, k):
-            self.A1[i] = A1prime[i-n] 
-
-        ident = np.identity(l)
-        zer = np.zeros((l, n))
-        for i in range(0, l):
-            self.A2[i] = zer[i] 
-        for i in range(l, 2*l):
-            self.A2[i] = ident[i-l] 
-        for i in range(2*l, k):
-            self.A2[i] = A2prime[i-2*l]  
+        self.kappa = 36
+        self.sigma = math.floor(11 * self.kappa * 1 *
+                                math.sqrt(self.k * self.N))
+        self.A1 = __makeA1(self)
+        self.A2 = __makeA2(self)
+        self.A1A2 = np.concatenate((self.A1, self.A2))
 
     def __str__(self):
-        return "A1: " + str(self.A1) + "\n A2: " + str(self.A2)
+        return "A: " + str(self.A1A2)
 
-    def getL(self) -> int:
-        return self.l
-    
-    def checkRandomness(self, randomness: np.ndarray) -> bool:
+    def getRCommit(self) -> np.ndarray:
         """
-        Ensures that the randomness selected is within allowed bounds.
-        ___
-
-        #TODO: Decide if this should be made private or not. 
-        sigma = 11 * kappa * Beta * sqrt(k * N)
+        Generate a random polynomial vector bounded by S_{beta} of length k.
         """
-        
-        bound = 4 * self.sigma * math.sqrt(self.N)
-        sum = 0
-        for r in randomness:
-            sum += r**2
-        return math.sqrt(sum % self.q) <= bound
+        r = np.random.randint(-1, 2, size=self.k)
+        while lin.norm(r, np.inf) == 0:  # In case we get an all zero vector.
+            r = np.random.randint(-1, 2, size=self.k)
+        return r
 
-    def getR(self) -> np.ndarray:
-       # _, *dist = self.distributionValues
-        allowedNorm = False
-        randomness = np.zeros(self.k)
-        while not allowedNorm:
-            randomness = sampleUniform(self.k, 0, self.sigma, self.q)
-            allowedNorm = self.checkRandomness(randomness)
-        return randomness
+    def getROpen(self) -> np.ndarray:
+        bound = math.floor(4 * self.sigma * math.sqrt(self.N))
+        r = np.random.randint(-bound, bound, size=self.k)
+        while (lin.norm(r, 2) <= bound or lin.norm(r, 2) == 0):
+            r = np.random.randint(-bound, bound, size=self.k)
+        return np.reshape(r, (self.k, 1))
 
-    def commit(self, message: np.ndarray, r: np.ndarray) -> np.ndarray:
+    def getF(self):
+        c1 = self.__getChallenge()
+        c2 = self.__getChallenge()
+        while (np.array_equal(c1, c2)):
+            c1 = self.__getChallenge()
+        cDiff = np.subtract(c1, c2)
+        print("C is invertible in R_q:", lin.norm(cDiff, np.inf) <= 2)
+        return cDiff
+
+    def __getChallenge(self):
         """
-        Commit to a message with randomness r. 
+        We need a challenge in the ring R_q, with an l_inf norm of 1, 
+        and a l_1 norm of kappa.
         """
-        print("A1: ", self.A1)
-        print("A1 * r", np.dot(np.transpose(self.A1), r) % self.q)
-        C = np.transpose(np.hstack((self.A1, self.A2)))
-        print("C: ", C)
-        C = np.dot(C, r) % self.q
-        print("C: ", C)
-        message = np.transpose(np.hstack((np.zeros(self.n), message)))
-        print("mess", message)
-        return ((C + message) % self.q)
+        remainingOnes = self.kappa
+        c = np.zeros(self.kappa * 2)
+        while (remainingOnes > 0):
+            idx = np.random.randint(len(c))
+            if (c[idx]) == 0:
+                c[idx] = np.random.choice([-1, 1])
+                remainingOnes -= 1
+        return c
 
-    def open(self, C, r, message):
-        Xtend = np.zeros(self.n + self.l)
-        for i in range(self.n, self.n+self.l):
-            Xtend[i] = message[i-self.n]
-        f = [0]
-        while f[0] == 0:
-            f1 = sampleUniform(1, 0, 1, self.q)
-            f2 = sampleUniform(1, 0, 1, self.q)
-            f = [1]#np.subtract(f1, f2) % self.q 
-        A = np.hstack((self.A1, self.A2))
-        print(Xtend)
-        print(C)
-        print(f)
-        print(f * np.transpose(Xtend))
-        Comp1 = (np.dot(np.transpose(A), r) + (f * np.transpose(Xtend))) % self.q
-        Comp2 = (f * C) % self.q
-        equals = True
-        for i in range(len(Comp1)):
-            if Comp1[i] != Comp2[i]:
-                print(Comp1[i] - Comp2[i], i)
-                equals = False
-        return equals
+    def commit(self, x, r):
+        Ar = np.matmul(self.A1A2, r) % self.q
+        zerox = np.concatenate((np.zeros(self.n), x))
+        return np.add(Ar, zerox) % self.q
 
+    def open(self, C, r, x, f):
+        """
+        f * C = A1A2 * r + F * ZeroX
+        """
+        lhs = f * C % self.q
 
-def main():
-    B = CommitmentScheme()
-    l = B.getL()
-    message = np.array([1], ndmin=l)
-    randomness = B.getR()
-    print("Randomness: ", randomness)
-    com = B.commit(message, randomness)
-    print("Commitment: \n", com, "\n")
-    opening = B.open(com, randomness, message)
-    print("Open: \n", opening)
-    # B.commit(x)
+        Ar = np.matmul(self.A1A2, r) % self.q
+        zerox = np.concatenate((np.zeros(self.n), x))
+        fz = f * zerox
+        rhs = np.add(Ar, fz) % self.q
+
+        return np.array_equal(lhs, rhs)
+
+    def open2(self, C, r, x, f):
+        c1c2 = np.reshape(C, (2, 1))
+        lhs = f * c1c2 % self.q
+        c1f = np.dot(C[0], f) % self.q
+        Ar = np.matmul(self.A1A2, r) % self.q
+
+        print("A1r:", Ar[0], "\n c1f:", c1f)
+        print(Ar[0] == c1f)
+        zerox = np.reshape(np.concatenate((np.zeros(self.n), x)), (2, 1))
+        fZerox = f * zerox
+        # rhs = np.add(Ar, fZerox) % self.q
+        # print("LHS: ", lhs % self.q, lhs.shape)
+        # print("RHS:", rhs % self.q, rhs.shape)
+        # return np.array_equal(lhs % self.q, rhs % self.q)
+        # lhs = np.matmul(f, np.reshape(C, (2, 1))) % self.q
 
 
+def works():
+    """
+    Also, if an (honest) committer would like to simply open the commitment (without
+    giving a zero-knowledge proof), he can simply output the r, x from (7) and f = 1.
+    """
+    m = np.random.randint(C.q, size=C.l)
+    rCommit = C.getRCommit()
+    rOpen = C.getROpen()
+    randomR = [rCommit, rOpen]
+    idx = np.random.randint(2)
+    c2 = C.commit(m, randomR[idx])
+    print(c2)
+    open = C.open(c2, randomR[idx], m, [1])
+    return open
+
+
+def doesntWork():
+    m = np.random.randint(C.q, size=C.l)
+    rCommit = C.getRCommit()
+    rOpen = C.getROpen()
+    c2 = C.commit(m, rCommit)
+    open = C.open(c2, rCommit, m, C.getF())
+    return open
 
 
 if __name__ == "__main__":
-    main()
+    C = CommitmentScheme()
+    counts = dict()
+    for _ in range(100):
+        # open = doesntWork()
+        open = works()
+        counts[open] = counts.get(open, 0) + 1
+    print(counts)
---
+++ Distributions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def sampleUniform(n: int | tuple[int, int], mean, sigma, q) -> np.ndarray:
+    return np.random.randint(mean-sigma, mean+sigma+1, size=n) % q
+
+
+if __name__ == "__main__":
+    q = 2 ** 32 - 527
+    print(q)
+    x = sampleUniform((5, 3), (q-1)/2, (q-1)/2, q)
+    print(x)
+    print(x.shape)
