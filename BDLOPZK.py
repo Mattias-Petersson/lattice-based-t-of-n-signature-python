393d9e1afc090b325670edb7b082ee10b7c717d2
MaxAake <maxgustafsson.ugglum@gmail.com>
2024-02-27 14:38:37 +0100

    Working on proof of sum
    sum does not function yet

--- BDLOPZK.py
+++ BDLOPZK.py
@@ -42,7 +42,7 @@ class BDLOPZK:
         return bool(z_bounded and self.cypari(lhs == rhs))
     
     
-    def proof_of_linear_relation(self, r1, r2, g1, g2 = 1):
+    def proof_of_linear_relation(self, r1, r2, g1, g2 = [1]):
         y1 = self.cypari.Vec(
             self.polynomial.gaussian_array(
                 self.comm_scheme.k, self.comm_scheme.sigma
@@ -55,7 +55,7 @@ class BDLOPZK:
         )
         t1 = self.cypari(self.comm_scheme.A1 * self.cypari.mattranspose(y1))
         t2 = self.cypari(self.comm_scheme.A1 * self.cypari.mattranspose(y2))
-        u = self.cypari(g1 * self.comm_scheme.A2 * self.cypari.mattranspose(y1) - self.comm_scheme.A2 * self.cypari.mattranspose(y2))
+        u = self.cypari(g2 * self.comm_scheme.A2 * self.cypari.mattranspose(y1) - g1 * self.comm_scheme.A2 * self.cypari.mattranspose(y2))
         # TODO: This d should be a hash in order to be a Sigma protocol.
         d = self.comm_scheme.get_challenge()
         dr1 = self.cypari(d * r1)
@@ -64,24 +64,66 @@ class BDLOPZK:
         z2 = self.cypari.Vec(y2 + dr2)
         return (t1, t2, u, z1, z2, d)
     
-    def verify_proof_of_linear_relation(self, t1, t2, u, z1, z2, d, g1, c1, c2, g2 = 1):
+    def verify_proof_of_linear_relation(self, t1, t2, u, z1, z2, d, g1, c1, c2, g2 = [1]):
         if not (self.__verify_z_bound(z1) and self.__verify_z_bound(z2)) :
             return False
         lhs1 = self.cypari(self.comm_scheme.A1 *  self.cypari.mattranspose(z1))
         rhs1 = self.cypari(t1 + (d * c1[0][0]))
         lhs2 = self.cypari(self.comm_scheme.A1 *  self.cypari.mattranspose(z2))
         rhs2 = self.cypari(t2 + (d * c2[0][0]))
-        lhs3 = self.cypari(g1 * self.comm_scheme.A2 * self.cypari.mattranspose(z1) - self.comm_scheme.A2 * self.cypari.mattranspose(z2))
-        rhs3 = self.cypari((g1 * c1[0][1] - c2[0][1]) * d + u)
+        lhs3 = self.cypari(g2 * self.comm_scheme.A2 * self.cypari.mattranspose(z1) - g1 * self.comm_scheme.A2 * self.cypari.mattranspose(z2))
+        rhs3 = self.cypari((g2 * c1[0][1] - g1 * c2[0][1]) * d + u)
         print(self.cypari(lhs3 == rhs3))
         return bool(self.cypari(lhs1 == rhs1) and self.cypari(lhs2 == rhs2) and self.cypari(lhs3 == rhs3))
 
+    def proof_of_sum(self, r1, r2, r3, g1 = [1], g2 = [1]):
+        y1 = self.cypari.Vec(
+            self.polynomial.gaussian_array(
+                self.comm_scheme.k, self.comm_scheme.sigma
+            )
+        )
+        y2 = self.cypari.Vec(
+            self.polynomial.gaussian_array(
+                self.comm_scheme.k, self.comm_scheme.sigma
+            )
+        )
+        y3 = self.cypari.Vec(
+            self.polynomial.gaussian_array(
+                self.comm_scheme.k, self.comm_scheme.sigma
+            )
+        )
+        t1 = self.cypari(self.comm_scheme.A1 * self.cypari.mattranspose(y1))
+        t2 = self.cypari(self.comm_scheme.A1 * self.cypari.mattranspose(y2))
+        t3 = self.cypari(self.comm_scheme.A1 * self.cypari.mattranspose(y3))
+        u = self.cypari( g1 * self.comm_scheme.A2 * self.cypari.mattranspose(y1) + g2 * self.comm_scheme.A2 * self.cypari.mattranspose(y2) - self.comm_scheme.A2 * self.cypari.mattranspose(y3) )
+        # TODO: This d should be a hash in order to be a Sigma protocol.
+        d = self.comm_scheme.get_challenge()
+        dr1 = self.cypari(d * r1)
+        dr2 = self.cypari(d * r2)
+        dr3 = self.cypari(d * r3)
+        z1 = self.cypari.Vec(y1 + dr1)
+        z2 = self.cypari.Vec(y2 + dr2)
+        z3 = self.cypari.Vec(y3 + dr3)
+        return (t1, t2, t3, u, z1, z2, z3, d)
+    
+    def verify_proof_of_sum(self, t1, t2, t3, u, z1, z2, z3, d, g1, c1, c2, c3, g2 = [1]):
+        if not (self.__verify_z_bound(z1) and self.__verify_z_bound(z2)) :
+            return False
+        lhs1 = self.cypari(self.comm_scheme.A1 *  self.cypari.mattranspose(z1))
+        rhs1 = self.cypari(t1 + (d * c1[0][0]))
+        lhs2 = self.cypari(self.comm_scheme.A1 *  self.cypari.mattranspose(z2))
+        rhs2 = self.cypari(t2 + (d * c2[0][0]))
+        lhs3 = self.cypari(self.comm_scheme.A1 *  self.cypari.mattranspose(z3))
+        rhs3 = self.cypari(t3 + (d * c3[0][0]))
+        lhs4 = self.cypari(g1 * self.comm_scheme.A2 * self.cypari.mattranspose(z1) + g2 * self.comm_scheme.A2 * self.cypari.mattranspose(z2) - self.comm_scheme.A2 * self.cypari.mattranspose(z3) )
+        rhs4 = self.cypari((g1 * c1[0][1] + g2 * c2[0][1] - c3[0][1]) * d + u)
+        print(self.cypari(lhs4 == rhs4))
+        return bool(self.cypari(lhs1 == rhs1) and self.cypari(lhs2 == rhs2) and self.cypari(lhs3 == rhs3) and self.cypari(lhs4 == rhs4))
 
 def commit(C: CommitmentScheme, m):
     r = C.r_commit()
     c = C.commit(m, r)
     return c, r
-
 
 def main():
     CommScheme = CommitmentScheme()
@@ -95,12 +137,24 @@ def main():
         open = ZK.verify_proof_of_opening(c[0][0], *proof)
         proofs[open] = proofs.get(open, 0) + 1
     for _ in range(10):
-        m1 = ZK.polynomial.uniform_array(ZK.comm_scheme.l)
+        m = ZK.polynomial.uniform_array(ZK.comm_scheme.l)
         g1 = CommScheme.get_challenge()
-        c1, r1 = commit(CommScheme, m1)
-        c2, r2 = commit(CommScheme, cypari(g1 * m1))
-        proof = ZK.proof_of_linear_relation(r1, r2, g1)
-        open = ZK.verify_proof_of_linear_relation(*proof, g1, c1, c2)
+        g2 = CommScheme.get_challenge()
+        c1, r1 = commit(CommScheme, cypari(g1 * m))
+        c2, r2 = commit(CommScheme, cypari(g2 * m))
+        proof = ZK.proof_of_linear_relation(r1, r2, g1, g2 = g2)
+        open = ZK.verify_proof_of_linear_relation(*proof, g1, c1, c2, g2 = g2)
+        proofs[open] = proofs.get(open, 0) + 1
+    for _ in range(10):
+        m1 = ZK.polynomial.uniform_array(ZK.comm_scheme.l)
+        m2 = ZK.polynomial.uniform_array(ZK.comm_scheme.l)
+        g1 = CommScheme.get_challenge()
+        g2 = CommScheme.get_challenge()
+        c1, r1 = commit(CommScheme, m)
+        c2, r2 = commit(CommScheme, m2)
+        c3, r3 = commit(CommScheme, cypari(g1 * m1 + g2 * m2))
+        proof = ZK.proof_of_sum(r1, r2, r3, g1 = g1, g2 = g2)
+        open = ZK.verify_proof_of_sum(*proof, g1, c1, c2, c3, g2 = g2)
         proofs[open] = proofs.get(open, 0) + 1
     print(proofs)
 
