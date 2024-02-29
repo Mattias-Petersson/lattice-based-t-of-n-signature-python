d336f38df1ba2700c63f94718183d8a586195043
Mattias.Petersson <Mattias.Petersson93@gmail.com>
2024-02-29 15:51:10 +0100

    Finished refactoring methods used in BDLOP
    Refactoring is now done, using types to make it more clear what is
    happening and what depends on what. All pytests now break due to not
    being updated to match the new signatures, will be fixed later.

--- BDLOPZK.py
+++ BDLOPZK.py
@@ -20,14 +20,8 @@ class BDLOPZK:
                 return False
         return True
 
-    def __make_y(self):
-        return self.polynomial.gaussian_array(
-            self.comm_scheme.k, self.comm_scheme.sigma
-        )
-
-    def __d_sigma(self):
-        # TODO: This d should be a hash in order to be a Sigma protocol.
-        return self.comm_scheme.get_challenge()
+    def __verify_z_multiple(self, *args):
+        return all(self.__verify_z_bound(i.z) for i in args)
 
     def __verify_A1_z(self, proof: ProofOfOpenLinear, d):
         lhs = self.cypari(
@@ -35,6 +29,28 @@ class BDLOPZK:
         )
         rhs = self.cypari(proof.t + (d * proof.c[0][0]))
         return bool(self.cypari(lhs == rhs))
+
+    def __verify_A1_z_multiple(self, *args, d):
+        return all(self.__verify_A1_z(proof, d) for proof in args)
+
+    def __initial_check(self, *args, d):
+        z_bounded = self.__verify_z_multiple(*args)
+        a1_z = self.__verify_A1_z_multiple(*args, d=d)
+        return z_bounded and a1_z
+
+    def __make_y(self):
+        return self.polynomial.gaussian_array(
+            self.comm_scheme.k, self.comm_scheme.sigma
+        )
+
+    def __make_proof_open(self, y, d, r) -> ProofOfOpen:
+        t = self.cypari(self.comm_scheme.A1 * self.cypari.mattranspose(y))
+        z = self.cypari.Vec(y + d * r)
+        return ProofOfOpen(z, t)
+
+    def __d_sigma(self):
+        # TODO: This d should be a hash in order to be a Sigma protocol.
+        return self.comm_scheme.get_challenge()
 
     def proof_of_opening(self, r) -> tuple[ProofOfOpen, cypari2.gen.Gen]:
         y = self.__make_y()
@@ -44,20 +60,14 @@ class BDLOPZK:
         return ProofOfOpen(z, t), d
 
     def verify_proof_of_opening(self, c1, proof: ProofOfOpen, d) -> bool:
-        z_bounded = self.__verify_z_bound(proof.z)
+        if not self.__verify_z_bound(proof.z):
+            return False
         lhs = self.cypari(
             self.comm_scheme.A1 * self.cypari.mattranspose(proof.z)
         )
+        rhs = self.cypari(proof.t + d * c1)
 
-        dc1 = self.cypari(d * c1)
-        rhs = self.cypari(proof.t + dc1)
-
-        return bool(z_bounded and self.cypari(lhs == rhs))
-
-    def __make_proof_open(self, y, d, r) -> ProofOfOpen:
-        t = self.cypari(self.comm_scheme.A1 * self.cypari.mattranspose(y))
-        z = self.cypari.Vec(y + d * r)
-        return ProofOfOpen(z, t)
+        return bool(self.cypari(lhs == rhs))
 
     def proof_of_linear_relation(
         self, r1, r2, g1, g2
@@ -78,10 +88,7 @@ class BDLOPZK:
     def verify_proof_of_linear_relation(
         self, proof_one: ProofOfOpenLinear, proof_two: ProofOfOpenLinear, u, d
     ) -> bool:
-        list_of_proofs = [proof_one, proof_two]
-        if not all([self.__verify_z_bound(i.z) for i in list_of_proofs]):
-            return False
-        if not all([self.__verify_A1_z(proof, d) for proof in list_of_proofs]):
+        if not self.__initial_check(proof_one, proof_two, d=d):
             return False
         lhs = self.cypari(
             self.comm_scheme.A2
@@ -100,54 +107,57 @@ class BDLOPZK:
     def __make_t(self, y):
         return self.cypari(self.comm_scheme.A1 * self.cypari.mattranspose(y))
 
-    def __make_z_sum(self, y, d, r):
-        return self.cypari.Vec(y + d * r)
-
-    def proof_of_sum(self, r1, r2, r3, g1, g2, g3):
-        y1, y2, y3 = self.__make_y(), self.__make_y(), self.__make_y()
-        t1, t2, t3 = self.__make_t(y1), self.__make_t(y2), self.__make_t(y3)
+    def proof_of_sum(self, r1, r2, r3, g1, g2, g3) -> tuple[
+        tuple[ProofOfOpen, ProofOfOpen, ProofOfOpen],
+        cypari2.gen.Gen,
+        cypari2.gen.Gen,
+    ]:
         d = self.__d_sigma()
-        z1, z2, z3 = (
-            self.__make_z_sum(y1, d, r1),
-            self.__make_z_sum(y2, d, r2),
-            self.__make_z_sum(y3, d, r3),
+        y = tuple(self.__make_y() for _ in range(3))
+        r = r1, r2, r3
+        t = [self.__make_t(i) for i in y]
+        z = [self.cypari.Vec(y + d * r) for y, r in zip(y, r)]
+        proof = tuple[ProofOfOpen, ProofOfOpen, ProofOfOpen](
+            ProofOfOpen(z, t) for z, t in zip(z, t)
         )
         u = self.cypari(
             self.comm_scheme.A2
             * (
-                g1 * self.cypari.mattranspose(y1)
-                + g2 * self.cypari.mattranspose(y2)
-                - g3 * self.cypari.mattranspose(y3)
+                g1 * self.cypari.mattranspose(y[0])
+                + g2 * self.cypari.mattranspose(y[1])
+                - g3 * self.cypari.mattranspose(y[2])
             )
         )
-
-        return t1, t2, t3, u, z1, z2, z3, d
+        return proof, u, d
 
     def verify_proof_of_sum(
-        self, t1, t2, t3, u, z1, z2, z3, d, c1, c2, c3, g1, g2, g3
-    ):
-        if not (self.__verify_z_bound(z1) and self.__verify_z_bound(z2)):
+        self,
+        proof: tuple[ProofOfOpenLinear, ProofOfOpenLinear, ProofOfOpenLinear],
+        u: cypari2.gen.Gen,
+        d: cypari2.gen.Gen,
+    ) -> bool:
+        proof_one, proof_two, proof_three = proof
+        if not self.__initial_check(*proof, d=d):
             return False
-        equivalences = []
 
-        """equivalences.append(self.__verify_A1_z(z1, t1, d, c1[0][0]))
-        equivalences.append(self.__verify_A1_z(z2, t2, d, c2[0][0]))
-        equivalences.append(self.__verify_A1_z(z3, t3, d, c3[0][0]))"""
-
-        lhs4 = self.cypari(
+        lhs = self.cypari(
             self.comm_scheme.A2
             * (
-                g1 * self.cypari.mattranspose(z1)
-                + g2 * self.cypari.mattranspose(z2)
-                - g3 * self.cypari.mattranspose(z3)
+                proof_one.g * self.cypari.mattranspose(proof_one.z)
+                + proof_two.g * self.cypari.mattranspose(proof_two.z)
+                - proof_three.g * self.cypari.mattranspose(proof_three.z)
             )
         )
-        rhs4 = self.cypari(
-            (g1 * c1[0][1] + g2 * c2[0][1] - g3 * c3[0][1]) * d + u
+        rhs = self.cypari(
+            (
+                proof_one.g * proof_one.c[0][1]
+                + proof_two.g * proof_two.c[0][1]
+                - proof_three.g * proof_three.c[0][1]
+            )
+            * d
+            + u
         )
-        equivalences.append(self.cypari(lhs4 == rhs4))
-
-        return all(equivalences)
+        return bool(self.cypari(lhs == rhs))
 
 
 def commit(C: CommitmentScheme, m):
@@ -181,14 +191,20 @@ def linear_relation(comm_scheme: CommitmentScheme, ZK: BDLOPZK, cypari):
 def proof_of_sum(comm_scheme: CommitmentScheme, ZK: BDLOPZK, cypari):
     m1 = ZK.polynomial.uniform_array(ZK.comm_scheme.l)
     m2 = ZK.polynomial.uniform_array(ZK.comm_scheme.l)
-    g1 = comm_scheme.get_challenge()
-    g2 = comm_scheme.get_challenge()
-    g3 = comm_scheme.get_challenge()
+    g1, g2, g3 = [comm_scheme.get_challenge() for _ in range(3)]
     c1, r1 = commit(comm_scheme, cypari(g3 * m1))
     c2, r2 = commit(comm_scheme, cypari(g3 * m2))
     c3, r3 = commit(comm_scheme, cypari(g1 * m1 + g2 * m2))
-    proof = ZK.proof_of_sum(r1, r2, r3, g1, g2, g3)
-    open = ZK.verify_proof_of_sum(*proof, c1, c2, c3, g1, g2, g3)
+    proof, *rest = ZK.proof_of_sum(r1, r2, r3, g1, g2, g3)
+    proof = tuple[ProofOfOpenLinear, ProofOfOpenLinear, ProofOfOpenLinear](
+        ProofOfOpenLinear(c, g, proof=proof)
+        for c, g, proof in [
+            [c1, g1, proof[0]],
+            [c2, g2, proof[1]],
+            [c3, g3, proof[2]],
+        ]
+    )
+    open = ZK.verify_proof_of_sum(proof, *rest)
     return open
 
 
