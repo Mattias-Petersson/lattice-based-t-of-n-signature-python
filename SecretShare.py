c096a0fe96695c0d616a9bafc041fc0b3e1fb7a1
MaxAake <maxgustafsson.ugglum@gmail.com>
2024-02-29 11:08:51 +0100

    Shamir Secret Sharing for Polynomials

--- SecretShare.py
+++ SecretShare.py
@@ -1,6 +1,7 @@
 import random
 
 from utils.Polynomial import Polynomial
+from cypari2.convert import gen_to_python
 import cypari2
 
 
@@ -16,13 +17,31 @@ def share(val, n, t, q):
     res_arr = []
     for i in range(1, n + 1):
         res_arr.append(cypari.centerlift(aPoly)(x=i))
-    return (res_arr, aPoly)
+    return res_arr
 
 
-def reconstruct(x_arr, aPoly):
-    cypari = cypari2.Pari()
+def share_poly(val, n, t, q):
+    ret_arr = []
+    for i in pol_to_arr(val):
+        ret_arr.append(share(i, n, t, q))
+    return ret_arr
+
+
+def reconstruct_poly(res_arrs, x_arr):
+    rec_arr = []
+    for i in range(len(res_arrs)):
+        internal = []
+        for j in range(len(res_arrs[0])):
+            internal.append(res_arrs[i][j])
+        rec_arr.append(internal)
+    ret_arr = []
+    for i in range(len(rec_arr)):
+        ret_arr.append(reconstruct(rec_arr[i], x_arr))
+    return cypari.Pol(ret_arr)
+
+
+def reconstruct(res_arr, x_arr):
     sum_arr = []
-    print(x_arr)
     for i in x_arr:
         sum = 0
         for j in x_arr:
@@ -31,9 +50,22 @@ def reconstruct(x_arr, aPoly):
         sum_arr.append(sum)
     resSum = 0
     for i in range(len(x_arr)):
-        resSum += sum_arr[i] * cypari.centerlift(aPoly)(x=x_arr[i])
+        resSum += int(sum_arr[i]) * res_arr[i]
     return resSum
 
 
-res_arr, aPoly = share(10, 4, 2, 2**32 - 527)
-print(reconstruct(res_arr[:2], aPoly))
+def pol_to_arr(pol) -> list[int]:
+    cypari = cypari2.Pari()
+    pariVec = cypari.Vec(cypari.liftall(pol))
+    return gen_to_python(pariVec)
+
+
+cypari = cypari2.Pari()
+res_arr = share(10, 4, 2, 2**32 - 527)
+print(res_arr)
+print(reconstruct(res_arr[:2], [1, 2]))
+ret_arr = share_poly(cypari.Pol("x^2+x^1+1"), 4, 2, 2**32 - 527)
+for i in range(len(ret_arr)):
+    ret_arr[i] = ret_arr[i][:2]
+print(ret_arr)
+print(reconstruct_poly(ret_arr, [1, 2]))
