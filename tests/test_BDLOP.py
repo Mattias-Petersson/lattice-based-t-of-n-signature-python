cef644f31935d7f571530167089099eb0e4dcfc8
Mattias.Petersson <Mattias.Petersson93@gmail.com>
2024-03-01 10:53:21 +0100

    Pytest proof of sum works

--- tests/test_BDLOP.py
+++ tests/test_BDLOP.py
@@ -73,3 +73,19 @@ def test_proof_of_linear(ZK, comm_scheme, poly, cypari):
         for c, g, proof in zip(c, g, proofs)
     )
     assert ZK.verify_proof_of_linear_relation(*proofs, u, d)
+
+
+def test_proof_of_sum(ZK, comm_scheme, poly, cypari):
+    m = tuple(poly.uniform_array(comm_scheme.l) for _ in range(2))
+    num: range = range(3)
+    g = tuple(comm_scheme.get_challenge() for _ in num)
+    r = tuple(comm_scheme.r_commit() for _ in num)
+    proof, *rest = ZK.proof_of_sum(*r, *g)
+    c1 = comm_scheme.commit(Commit(cypari(g[2] * m[0]), r[0]))
+    c2 = comm_scheme.commit(Commit(cypari(g[2] * m[1]), r[1]))
+    c3 = comm_scheme.commit(Commit(cypari(g[0] * m[0] + g[1] * m[1]), r[2]))
+    proof = tuple[ProofOfOpenLinear, ProofOfOpenLinear, ProofOfOpenLinear](
+        ProofOfOpenLinear(c, g, proof=proof)
+        for c, g, proof in zip((c1, c2, c3), g, proof)
+    )
+    assert ZK.verify_proof_of_sum(proof, *rest)
