f9e07f4aa4135b7a9232f20edcef1d828336895a
Mattias.Petersson <Mattias.Petersson93@gmail.com>
2024-04-09 18:36:52 +0200

    GKS23 KGen
    GKS23 now supports key generation. Much like with BGV, the KGen returns
    the list of active participants, rather than the public key secret key
    pairs.

--- GKS23/GKS.py
+++ GKS23/GKS.py
@@ -1,20 +1,22 @@
+from typing import Iterable
 from BDLOP16.CommitmentScheme import CommitmentScheme
 from BGV122.BGV import BGV
 from GKS23.GKSParticipant import GKSParticipant
 from Models.Controller import Controller
+from Models.values import default_values
 from SecretSharing.SecretShare2 import SecretShare
-from type.classes import BGVValues
+from type.classes import BGVValues, Sk, poly
 from utils.Polynomial import Polynomial
 
 
 class GKS(Controller):
     def __init__(
         self,
-        q: int = 2**32 - 527,
-        p: int = 2029,
-        N: int = 1024,
-        t: int = 3,
-        n: int = 5,
+        q: int,
+        p: int,
+        N: int,
+        t: int,
+        n: int,
     ):
         self.q = q
         self.p = p
@@ -27,8 +29,15 @@ class GKS(Controller):
         self.cypari = self.comm_scheme.cypari
         self.secret_share = SecretShare((self.t, self.n), self.q)
         self.participants: tuple[GKSParticipant, ...] = tuple(
-            GKSParticipant(self.comm_scheme, self.secret_share, self.p)
-            for _ in range(n)
+            GKSParticipant(
+                self.comm_scheme,
+                self.secret_share,
+                self.q,
+                self.p,
+                self.N,
+                i + 1,
+            )
+            for i in range(n)
         )
         bgv_values = BGVValues(
             self.participants,
@@ -38,28 +47,46 @@ class GKS(Controller):
             self.n,
         )
         self.BGV = BGV(bgv_values, p, q, N)
+        super().__init__(self.participants)
 
-        """self.participants = [
-            GKSParticipant(
-                self.BGV.comm_scheme, self.BGV.secret_share, self.BGV.p
-            )
-        ]"""
+    def __KGen_step_2(self):
+        self.assert_value_matches_hash("a")
+        for p in self.participants:
+            p.KGen_step_2()
+
+    def __KGen_step_3(self):
+        for p in self.participants:
+            p.KGen_step_3()
+        self.recv_share("y")
+        self.recv_share("ctx_s")
+
+    def __finalize(self):
+        self.assert_value_matches_hash("y")
+        for part in self.participants:
+            part.KGen_step_4()
+        return self.participants
 
     def KGen(self):
-        print()
+        """
+        KGen of actively secure GKS23. Step one is init BGV, which is done in
+        the constructor and is thus not included here.
+        """
+        self.__KGen_step_2()
+        self.__KGen_step_3()
+        return self.__finalize()
 
 
 if __name__ == "__main__":
-    gks = GKS()
-    public_keys, secret_keys = gks.BGV.DKGen()
+    gks = GKS(**default_values)
+    bgv_participants = gks.BGV.DKGen()
+    part = next(iter(bgv_participants))
     results = dict()
+    participants = gks.KGen()
     for _ in range(100):
         m = gks.BGV.get_message()
-        u, v = gks.BGV.enc(m)
-
-        d = gks.BGV.t_dec(secret_keys, u)
-        decrypted = gks.BGV.comb(v, d)
+        ctx = part.enc(m)
+        d = gks.BGV.t_dec(bgv_participants[: gks.BGV.t], ctx)
+        decrypted = part.comb(ctx, d)
         res = decrypted == m
         results[res] = results.get(res, 0) + 1
     print(results)
-    print([i.name for i in gks.participants])
