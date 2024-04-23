b6a72bbc4216580c2405586921ece004e24f3157
Mattias.Petersson <Mattias.Petersson93@gmail.com>
2024-04-23 12:01:39 +0200

    Refactoring of Polynomials class
    Found some slight optimizations in the polynomials class when
    calculating a sum. Took some time to add docstrings to methods that are
    publicly available and in use.

--- utils/Polynomial.py
+++ utils/Polynomial.py
@@ -1,8 +1,7 @@
-import cypari2
-from numpy.random import Generator, PCG64
 import math
 import hashlib
-
+import cypari2
+from numpy.random import Generator, PCG64
 from type.classes import poly
 
 
@@ -46,8 +45,8 @@ class Polynomial:
         This distribution is used instead of a discrete Gaussian distribution.
         """
         unrounded = self.gen.normal(0, sigma, size=self.N)
-        poly = self.cypari.round(self.cypari.Pol(unrounded))
-        return self.in_rq(poly)
+        pol = self.cypari.round(self.cypari.Pol(unrounded))
+        return self.in_rq(pol)
 
     def in_rq(self, p: poly):
         """
@@ -66,6 +65,10 @@ class Polynomial:
         )
 
     def basis_poly(self) -> poly:
+        """
+        Returns the basis polynomial for this instance. The basis polynomial
+        is going to be in the form "x^N + 1", where N is a power of two.
+        """
         fx = f"x^{self.N} + 1"
         return self.cypari.Pol(fx)
 
@@ -76,7 +79,10 @@ class Polynomial:
         specified dimension if n is a tuple. This helper method takes in
         functions to get the element and returns the proper structure.
         """
-        func_mult = lambda n, bound: [func_one_element(bound) for _ in range(n)]
+
+        def func_mult(n, bound):
+            return [func_one_element(bound) for _ in range(n)]
+
         if isinstance(n, int):
             return lambda n, bound: (
                 func_one_element(bound)
@@ -90,18 +96,48 @@ class Polynomial:
     def uniform_array(
         self, n: int | tuple[int, int], bound: int = 0
     ) -> poly | list[poly]:
+        """
+        Returns a list of polynomials, where each polynomial is created
+        according to a uniform distribution.
+
+        Args:
+        n (int | tuple[int, int]): The shape of the output. If it is 1,
+        one element is returned as if we were calling on uniform_element.
+        If it is an integer other than 1, an array gets sent back. If a tuple
+        is sent in, a matrix is returned.
+
+        bound (int): the upper bound for the polynomials
+        """
         func = self.__shape_helper(n, self.uniform_element)
         return func(n, bound)
 
     def gaussian_array(
         self, n: int | tuple[int, int], sigma: int
     ) -> poly | list[poly]:
+        """
+        Returns a list of polynomials, where each polynomial is created
+        according to a Gaussian distribution.
+
+        Args:
+        n (int | tuple[int, int]): The shape of the output. If it is 1,
+        one element is returned as if we were calling on uniform_element.
+        If it is an integer other than 1, an array gets sent back. If a tuple
+        is sent in, a matrix is returned.
+
+        sigma (int): The standard deviation to use
+        """
         func = self.__shape_helper(n, self.gaussian_element)
         return func(n, sigma)
 
     def guassian_bounded_array(
         self, n: int | tuple[int, int], sigma: int, bound: int
     ) -> poly | list[poly]:
+        """
+        Creates a list of polynomials according to a Gaussian distribution,
+        then checks so that this falls within a certain bound for the values.
+        If it doesn't, a new one is created until it satisfies.
+        """
+
         def bounded_element(sigma):
             r = self.gaussian_element(sigma)
             while self.l2_norm(r) > bound:
@@ -112,22 +148,25 @@ class Polynomial:
         return func(n, sigma)
 
     def ones(self, n: int) -> list[poly]:
+        """
+        Creates an identity matrix in the polynomial's space.
+        """
         return self.in_rq(self.cypari.matid(n))
 
-    def l2_norm(self, poly: poly) -> float:
+    def l2_norm(self, pol: poly) -> float:
         """
         Takes the l2 norm of a polynomial by first converting it to
         an integer array.
         """
-        lst = self.pol_to_arr(poly)
-        return math.sqrt(sum([i**2 % self.q for i in lst]))
+        lst = self.pol_to_arr(pol)
+        return math.sqrt(sum(i**2 % self.q for i in lst))
 
     def pol_to_arr(self, pol: poly) -> list[int]:
         """
         Convert a polynomial to an array of integers.
         """
-        pariVec = self.cypari.Vec(self.cypari.liftall(pol))
-        return pariVec
+        pari_vec = self.cypari.Vec(self.cypari.liftall(pol))
+        return pari_vec
 
     def challenge(self, kappa: int, seed: list[int] | None = None) -> poly:
         """
@@ -147,6 +186,10 @@ class Polynomial:
         return self.cypari.Pol(pol)
 
     def challenge_vector(self, n: int, kappa: int):
+        """
+        Returns a vector of small challenges. These challenges all have an
+        l_inf norm of one. Calls the challenge method for n entries.
+        """
         return self.cypari.vector(n, [self.challenge(kappa) for _ in range(n)])
 
     def small_invertible(self, kappa: int) -> poly:
@@ -168,7 +211,7 @@ class Polynomial:
         h = hashlib.sha384()
         for i in args:
             h.update(str.encode(str(i)))
-        integers_hash: list[int] = [i for i in h.digest()]
+        integers_hash: list[int] = list(h.digest())
         return self.challenge(kappa=kappa, seed=integers_hash)
 
 
