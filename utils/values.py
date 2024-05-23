afe07a13d009e4d075f58eaf2c4f8c517c48642b
Mattias.Petersson <Mattias.Petersson93@gmail.com>
2024-05-23 14:50:35 +0200

    Update q, Q to be equal to 17 mod 32
    We had a slight miss where we missed that the q and Q were required to
    be equal to 2d + 1 mod 4d, with d either 2, 4, or 8. We set d to be 8
    and found new q, Q values. Tested and functionality still works.

--- utils/values.py
+++ utils/values.py
@@ -1,5 +1,5 @@
 default_values = {
-    "q": 2**24 - 3,
+    "q": 2**24 - 879,
     "p": 2**16 - 39,
     "N": 2**10,
     "tn": (3, 5),
@@ -7,11 +7,12 @@ default_values = {
 }
 
 single_use_values = {
-    "q": 2**20 - 5,
+    "q": 2**20 - 143,
     "p": 2**16 - 39,
     "N": 2**10,
     "tn": (3, 5),
     "sigma": 2**11,
 }
 # Q is a large ciphertext modulus.
-Q = 2**36 - 5
+Q_single = 2**35 - 975
+Q_multiple = 2**36 - 527
